df36924036fb803e46fa5acf27afe421b0209b48
AlexsanderGS <alexsanderga@gmail.com>
2023-09-16 15:36:11 -0300

    Criando o pacote Entity e as classes Times e Jogos

---
+++ src/main/java/br/edu/cbf/campeonatobrasileiro/entity/Jogo.java
@@ -0,0 +1,23 @@
+package br.edu.cbf.campeonatobrasileiro.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Jogo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private Time time1;
+	private Time time2;
+	private Integer golsTime1;
+	private Integer golsTime2;
+	private Integer publicoPagante;
+	private String uuid;
+
+}
---
+++ src/main/java/br/edu/cbf/campeonatobrasileiro/entity/Time.java
@@ -0,0 +1,20 @@
+package br.edu.cbf.campeonatobrasileiro.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Time {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String sigla;
+	private String uf;
+
+}
